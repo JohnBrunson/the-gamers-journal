4e7842f63f7d7bfea2216c068b658cec4defc68f
Finnmos1 <ljones7359@gmail.com>
2024-11-18 16:52:19 -0700

    fixed createDataObject, removed MP from all functions as it is redundent with seperate JS

--- assets/js/multiplayer.js
+++ assets/js/multiplayer.js
@@ -1,50 +1,51 @@
 // Yoinking to test info
+// 100% should be change once we have more time
 window.addEventListener("load", (event) => {
-    let mpGames = getMPLocalStorage();
+    let games = getLocalStorage();
     const dropdown = document.querySelector('#dropdown');
-    let mpGameTitleList;
-    let mpGameTitleListInterim = [];
-    if (mpGames.length === 0) {
+    let gameTitleList;
+    let gameTitleListInterim = [];
+    if (games.length === 0) {
         console.log("INFO: No Games Found. Add some to help this application achieve glorious domination!")
     }else {
         //Collect all the game titles
-        for (i = 0; i < mpGames.length; i++){
-            mpGameTitleListInterim.push(mpGames[i].gameTitle);
+        for (i = 0; i < games.length; i++){
+            gameTitleListInterim.push(games[i].gameTitle);
             //run a duplicate check
             //Effectively, it appears any set MUST have unique values. Converting the array to a set and then back again removes the duplicates.
-            mpGameTitleList = [... new Set(mpGameTitleListInterim)];
+            gameTitleList = [... new Set(gameTitleListInterim)];
         }
         //render to the game list. This may need to be its own separate function. For MVP, it's probably fine.
-        if (mpGames.length === 0){
+        if (games.length === 0){
             console.log("INFO: No Games Found. Add some to help this application achieve glorious domination!")
         }else {
-            for (i = 0; i < mpGameTitleList.length; i++){
-                const mpGame = document.createElement('option')
-                mpGame.textContent = mpGameTitleList[i];
-                console.log (`INFO: mpGame.textContent: ${mpGame.textContent} should render to the screen`)
-                dropdown.appendChild(mpGame);
-                mpGame.setAttribute('id', `localstorage${i}`)
+            for (i = 0; i < gameTitleList.length; i++){
+                const game = document.createElement('option')
+                game.textContent = gameTitleList[i];
+                console.log (`INFO: game.textContent: ${game.textContent} should render to the screen`)
+                dropdown.appendChild(game);
+                game.setAttribute('id', `localstorage${i}`)
         }
 
         }
     }
 })
 
-function getMPLocalStorage() {
-    let mpGames = JSON.parse(localStorage.getItem('spGames'));
-    if (!spGames) {
-        console.log ("INFO: No SP Games Found, should return empty array.")
+function getLocalStorage() {
+    let games = JSON.parse(localStorage.getItem('games'));
+    if (!games) {
+        console.log ("INFO: No Games Found, should return empty array.")
         return [];
     }
-    return mpGames;
+    return games;
 }
 
-function setMPLocalStorage(item) {
-    let items = getMPLocalStorage();
+function setLocalStorage(item) {
+    let items = getLocalStorage();
     //push new item to items
     items.push(item);
     //save data to local storage after stringifying it.
-    localStorage.setItem('mPitems', JSON.stringify(items));
+    localStorage.setItem('items', JSON.stringify(items));
 }
 
 function domAppend(el, parent) {
@@ -55,37 +56,37 @@ function domAppend(el, parent) {
 }
 
 
-function createMPDataObject(event) {
+function createDataObject(event) {
     event.preventDefault();
-    console.log("INFO: createMPDataObject was called.")
+    console.log("INFO: createDataObject was called.")
+    
+    if (gameTitleModal.value === "" || gameRatingModal.value === ""){ 
+        const main = document.querySelector('#modal-body');
+        const paragraphEl = domAppend('p', modalBody)
+        paragraphEl.setAttribute('id', 'error')
+        const error = document.querySelector('#error')
+        error.textContent = "Please complete the required fields."
+    }else {
+        const gameTitle = document.querySelector('#gameTitle');
+        const gameRating = document.querySelector('#gameRating');
+        const wins = document.querySelector('#wins');
+        const losses = document.querySelector('#losses');
+        let journalEntry = {
+            gameTitle : gameTitle.value,
+            gameRating : rating.value,
+            wins : wins.value,
+            losses : losses.value
+        };
+        setLocalStorage(journalEntry);
+        return journalEntry;
+    }
 }
 
-const mpGameTitle = document.querySelector('#mpGameTitle');
-const mpRating = document.querySelector('#mpRating');
-const mpWins = document.querySelector('#mpWins');
-const mpLosses = document.querySelector('#mpLosses');
-
-if (mpGameTitle.value === "" || mpRating.value === "" || mpWins.value === "" || mpLosses.value){
-    // return error message -- 
-    const main = document.querySelector('main');
-    const paragraphEl = domAppend('p', main)
-    paragraphEl.setAttribute('id', 'error')
-    const error = document.querySelector('#error')
-    error.textContent = "Please complete the required fields."
-}else {
-    let mPJournalEntry = {
-        mpGameTitle : mpGameTitle.value,
-        mpRating : mpRating.value,
-        mpWins : mpWins.value,
-        mpLosses : mpLosses.value
-    };
-    setMPLocalStorage(mPJournalEntry);
-    return mPJournalEntry;
-}
-   
+let saveBtn = document.querySelector('gameForm')
+saveBtn.addEventListener('submit', createDataObject)
 
 //MP = Multiplayer (Competitve) -- Bowman
-// function createMPDataObject {
+// function createDataObject {
 // //     Name of Game
 // // Wins (Number)
 // // Losses (Number)
@@ -96,8 +97,8 @@ if (mpGameTitle.value === "" || mpRating.value === "" || mpWins.value === "" || 
 
 // rendering -- Leo
 
-function renderMPBlogPost() {
-    const numberOfJournals = getMPLocalStorage.length;
+function renderBlogPost() {
+    const numberOfJournals = getLocalStorage.length;
     for (let i = 0; i > numberOfJournals; i++) {
         const main = document.querySelector('main');
         const recordDay = domAppend('div', main);
